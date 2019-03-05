aba657ff69a8a254141180f5d5e376d173630928
zhangjingcheng <zhangjingcheng000@gmail.com>
2019-03-06 01:24:14 +0800

    新建子模块，配置pom

---
+++ consumer/src/main/java/com/zjc/dubbo/App.java
@@ -0,0 +1,13 @@
+package com.zjc.dubbo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
